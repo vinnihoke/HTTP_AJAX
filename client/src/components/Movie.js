29927dbe0d890c6277d96a0f65256e133393a28c
Vinni Hoke <vinni.hoke@gmail.com>
2019-10-13 14:40:18 -0700

    MVP Completed

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -32,6 +32,13 @@ export default class Movie extends React.Component {
     addToSavedList(this.state.movie);
   };
 
+  deleteMovie = () => {
+    axios
+      .delete(`http://localhost:5000/api/movies/${this.state.movie.id}`)
+      .then(res => this.props.history.push(`/`))
+      .catch(err => console.log(err));
+  };
+
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
@@ -49,6 +56,7 @@ export default class Movie extends React.Component {
         >
           <button>Update</button>
         </Link>
+        <button onClick={this.deleteMovie}>Delete</button>
       </>
     );
   }
